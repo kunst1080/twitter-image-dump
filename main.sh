71dccd971437f6aeb2706d2a3cbb7994c6629e37
kunst1080 <kontrapunkt1080@gmail.com>
2017-06-05 23:26:58 +0900

    ダウンロード済みのファイルはダウンロードしない

--- main.sh
+++ main.sh
@@ -5,11 +5,12 @@ set -eu
 mkdir -p .tmp
 cat list.txt | xargs python3 collect.py true > .tmp/download.txt
 
-cat .tmp/download.txt | while read ID USER URL
+grep -v -f .tmp/downloaded.txt .tmp/download.txt | while read ID USER URL
 do
     FILENAME=$(basename $URL)
     OUTPUT=out/$USER
     echo $ID, $USER, $URL
     mkdir -p $OUTPUT
     wget "$URL:large" -O $OUTPUT/$FILENAME
+    echo $ID >> .tmp/downloaded.txt
 done 2>> log.txt
